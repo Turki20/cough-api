1bb3c954f976b5bcb3e9ab861c72dade8040feeb
turki ahmed <turki202mt@gmail.com>
2025-05-07 12:16:28 +0300

    update url

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UploadAudioView, TrainModelView, PredictView, FFmpegCheckView
+from .views import UploadAudioView, TrainModelView, PredictView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -7,7 +7,6 @@ urlpatterns = [
     path("upload/", UploadAudioView.as_view()),
     path("train/", TrainModelView.as_view()),
     path("predict/", PredictView.as_view()),
-    path('check-ffmpeg/', FFmpegCheckView.as_view()),
 ]
 if settings.DEBUG:  # هذا فقط في بيئة التطوير
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
